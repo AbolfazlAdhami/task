8266b7edb9d6af86150ed680b813732c66185207
AbolfazlAdhami <aadhami880@gmail.com>
2025-05-05 14:12:18 +0330

    config: tailwind config and antd

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+function App() {
+  return (
+    <section className="contain-content flex ">
+      <h1 className="text-2xl font-mono ">Domains</h1>
+    </section>
+  );
+}
+
+export default App;
---
+++ src/components/Button.tsx
@@ -0,0 +1,10 @@
+import { Button } from "antd";
+
+export default function App() {
+  return (
+    <div className="p-8 bg-gray-100 min-h-screen">
+      <h1 className="text-2xl font-bold mb-4">My SPA</h1>
+      <Button type="primary">Ant Design Button</Button>
+    </div>
+  );
+}
